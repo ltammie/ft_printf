41df300a0c77c37e86ebf22d6c111ff4abe3959f
Igor <sauron@MacBook-Pro-Igor.local>
2020-07-22 21:14:13 +0300

    dddd

--- di_pr.c
+++ di_pr.c
@@ -1,49 +1,127 @@
-#include "includes/ft_printf.h"
+//#include "includes/ft_printf.h"
+//
+//static void print_arg(t_cp *z, char *s, int len)
+//{
+//	if (s[0] == '-')
+//	{
+//		s = ft_strsub(s, 1, len - 1);
+//		len = (int)ft_strlen(s);
+//		ft_putchar('-');
+//		if (z->precision >= 0)
+//			print_width(z->precision - len, '0');
+//		ft_putstr(s);
+//	}
+//	else if (z->plus_flag == 1)
+//	{
+//		ft_putchar('+');
+//		len--;
+//		if (z->precision >= 0)
+//			print_width(z->precision - len, '0');
+//		ft_putstr(s);
+//	}
+//	else
+//	{
+//		if (z->precision >= 0)
+//			print_width(z->precision - len, '0');
+//		ft_putstr(s);
+//	}
+//}
+//
+//static void print_min_flag(t_cp *z, char *s, int len)
+//{
+//	if (z->minus_flag == 1)
+//	{
+//		print_arg(z, s, len);
+//		print_width(z->precision > len ?  z->width - z->precision : z->width - len, ' ');
+//	}
+//	else
+//	{
+//		print_width(z->precision > len ?  z->width - z->precision : z->width - len, z->zero_flag ? '0' : ' ');
+//		print_arg(z, s, len);
+//	}
+//}
+//
+//int			di_pr(t_cp *z, va_list ap)
+//{
+//	char			*s;
+//	int				len;
+//	s = ft_itoa(va_arg(ap, int));
+//	if (ft_strcmp(s, "0") == 0 && z->precision == 0)
+//	{
+//		z->width > 0 ? print_width(z->width, ' ') : ft_putstr("");
+//		return (0);
+//	}
+//	len = (int)ft_strlen(s);
+//	if (z->plus_flag == 1 && s[0] != '-')
+//		z->width--;
+//	if (s[0] == '-')
+//		z->plus_flag = 0;
+//	print_min_flag(z, s, len);
+//	return (0);
+//}
 
-static void print_arg(t_cp *z, char *s, int len)
-{
-	if (s[0] == '-')
-	{
-		s = ft_strsub(s, 1, len - 1);
-		ft_putchar('-');
-	}
-	if (z->plus_flag == 1)
-		ft_putchar('+');
-	if (z->precision >= 0)
-		print_width(z->precision - len, '0');
-	ft_putstr(s);
-}
+#include "includes/ft_printf.h"
 
 static void print_min_flag(t_cp *z, char *s, int len)
 {
+	len = ft_strlen(s);
 	if (z->minus_flag == 1)
 	{
-		print_arg(z, s, len);
-		print_width(z->precision > len ?  z->width - z->precision : z->width - len, ' ');
+		ft_putstr(s);
+		print_width(z->width - len, ' ');
 	}
 	else
 	{
-		print_width(z->precision > len ?  z->width - z->precision : z->width - len, z->zero_flag ? '0' : ' ');
-		print_arg(z, s, len);
+		print_width(z->width - len, z->zero_flag ? '0' : ' ');
+		ft_putstr(s);
+	}
+}
+
+static void print_0p_0v(char *s, t_cp *z, int len)
+{
+	s = ft_strdup("");
+	len = (int)ft_strlen(s);
+	if (z->minus_flag == 1)
+	{
+		ft_putstr(s);
+		print_width(z->width - len, ' ');
+	}
+	else
+	{
+		print_width(z->width - len, z->zero_flag ? '0' : ' ');
+		ft_putstr(s);
 	}
 }
 
 int			di_pr(t_cp *z, va_list ap)
 {
 	char			*s;
+	char 			*tmp;
+	char 			*sign;
 	int				len;
 
 	s = ft_itoa(va_arg(ap, int));
+	sign = ft_strdup("");
+	if (z->plus_flag == 1)
+		sign = ft_strdup("+");
+	if (s[0] == '-')
+	{
+		sign = ft_strdup("-");
+		s = ft_strsub(s, 1, (int)ft_strlen(s) - 1);
+	}
+	len = (int)ft_strlen(s) - (int)ft_strlen(sign);
 	if (ft_strcmp(s, "0") == 0 && z->precision == 0)
 	{
-		z->width > 0 ? print_width(z->width, ' ') : ft_putstr("");
+		print_0p_0v(s, z, len);
 		return (0);
 	}
-	len = (int)ft_strlen(s);
-	if (z->plus_flag == 1 && s[0] != '-')
-		z->width--;
-	if (s[0] == '-')
-		z->plus_flag = 0;
+	if (len < z->precision)
+	{
+		tmp = (char *)malloc(sizeof(char) * (z->precision - len));
+		s = ft_strjoin(ft_memset(tmp, '0', z->precision - len), s);
+		free(tmp);
+	}
 	print_min_flag(z, s, len);
 	return (0);
 }
+
--- main.c
+++ main.c
@@ -5,7 +5,10 @@ int 	main(void)
 //	ft_printf("mine str|%-*.1s\n", 3, "ez");
 //	printf("orig str|%-*.1s\n", 3, "ez");
 
-	ft_printf("mine str|%#.3x\n", 0);
-	printf("orig str|%#.3x\n", 0);
+//	ft_printf("mine str|%-20.3s\n", "abcde");
+//	printf("orig str|%-20.3s\n", "abcde");
+
+	ft_printf("mine str|%+10.0d\n", 0);
+	printf("orig str|%+.0d\n", 0);
 	return (0);
 }
